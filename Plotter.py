83931ada749c5032769f4ecd39ee6e737f624d83
yesoer <luisenglaender@web.de>
2021-10-12 00:06:01 +0200

    initalize plotter

---
+++ Plotter.py
@@ -0,0 +1,42 @@
+import json
+from Scraper import Scraper
+from os.path import exists
+import getopt
+import sys
+
+# process arguments
+(OPT, _) = getopt.getopt(sys.argv[1:], "cdu:p:", ["cache", "debug", "username", "password"])
+OPT = dict(OPT)
+
+DEBUG = '-d' in OPT or '--debug' in OPT
+USE_CACHE = '-c' in OPT or '--cache' in OPT
+USERNAME = OPT['-u'] if OPT['-u'] != '' else OPT['--username']   
+PASSWORD = OPT['-p'] if OPT['-p'] != '' else OPT['--password']
+
+def get_data() -> dict:
+    data = {}
+
+    if not USE_CACHE or not exists('./data.json'):
+        # refetch data
+        scraper = Scraper(DEBUG)
+        data = scraper.fetch_data(USERNAME, PASSWORD)
+
+        if USE_CACHE:
+            # cache data
+            data_json = json.dumps(data, indent=4)
+
+            with open('data.json', 'w') as f:
+                f.write(data_json)
+    else:
+        # get catched data
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+
+    return data
+
+def main():
+    data = get_data()
+    print("data", data)
+
+if __name__ == '__main__':
+    main()
