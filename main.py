a2b472d4748bbbfb3e69483ab1bcb533213b717e
yesoer <luisenglaender@web.de>
2021-10-22 00:08:23 +0200

    rework caching behavior, remove the option that handled this
    now if possible cached data is loaded and new data is fetched in the background
    if no cached data is available, one has to wait for the initial fetch

--- main.py
+++ main.py
@@ -21,35 +21,40 @@ class Plotter(App):
     def build(self):
 
         # get data which is a list of dictionaries, sorted by entry date
-        scraper = Scraper(DEBUG)  # TODO : run this in background thread
-        # TODO : await until scraper has been initalized
-        data = self.get_data(scraper)
-        print("data", data)
+        potential_thread = None
 
-        Builder.load_file("app.kv")
-
-        return Plot(data)
-
-    def get_data(self, scraper) -> dict:
-        data = {}
-
-        if not USE_CACHE or not exists('./data.json'):
-            # refetch data
-            data = scraper.fetch_data(USERNAME, PASSWORD)
-
-            if USE_CACHE:
-                # cache data
-                data_json = json.dumps(data, indent=4)
-
-                with open('data.json', 'w') as f:
-                    f.write(data_json)
-        else:
+        if exists(CACHE_PATH):
             # get catched data
             with open('data.json', 'r') as f:
-                data = json.load(f)
+                self.data = json.load(f)
 
-        return data
+            potential_thread = threading.Thread(target=self.init_scraper, name="Scraper-Initalizer")
+            potential_thread.start()
 
+        else:
+            self.init_scraper()
+
+        # get view
+        Builder.load_file("app.kv")
+
+        return Plot(self.data)
+
+
+    def init_scraper(self):
+        """Needs to be called to initalize the scraper, after refetching can be done by simply calling 'fetch_data' with 'self.scraper' as argument"""
+        self.scraper = Scraper(DEBUG) 
+        self.fetch_data()
+        print("Initalization done", len(self.data), self.data)
+
+    def fetch_data(self) -> dict:        
+        # refetch data
+        self.data = self.scraper.fetch_data(USERNAME, PASSWORD)
+        
+        # cache data
+        data_json = json.dumps(self.data, indent=4)
+
+        with open('data.json', 'w') as f:
+            f.write(data_json)
 
 if __name__ == '__main__':
     app = Plotter()
